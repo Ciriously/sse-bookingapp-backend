f1390bf6996ccfa6f3a277648bd60a3049ec49cd
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2024-06-30 03:56:39 +0530

    orssss

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-// require("dotenv").config();
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
-// const userRoutes = require("./routes/user");
-// const mongoose = require("mongoose");
-// const trainRouters = require("./routes/train");
-// const port = process.env.PORT || 4000;
-
-// mongoose.connect(process.env.MONGODB_URI);
-// mongoose.connection.on("connected", () => {
-//   console.log("Connected to MongoDB");
-// });
-// mongoose.connection.on("error", (err) => {
-//   console.log("Failed to connect to MongoDB", err);
-// });
-// const corsOptions = {
-//   origin: process.env.CORS_ORIGIN,
-// };
-
-// app.use(cors(corsOptions));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use("/", userRoutes);
-// app.use("/admin", trainRouters);
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -45,19 +16,20 @@ mongoose.connection.on("connected", () => {
 mongoose.connection.on("error", (err) => {
   console.log("Failed to connect to MongoDB", err);
 });
+
 app.use(
   cors({
-    origin: ["https://railyatri.vercel.app"],
-    methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
+    origin: "https://railyatri.vercel.app",
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", userRoutes);
 app.use("/admin", trainRouters);
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-// https://railyatri.vercel.app/
