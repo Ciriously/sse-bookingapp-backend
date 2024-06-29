2b01e69eae5528de1d08174186aa954c9f152483
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2024-06-30 02:59:56 +0530

    final changes

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+// require("dotenv").config();
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// const userRoutes = require("./routes/user");
+// const mongoose = require("mongoose");
+// const trainRouters = require("./routes/train");
+// const port = process.env.PORT || 4000;
+
+// mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connection.on("connected", () => {
+//   console.log("Connected to MongoDB");
+// });
+// mongoose.connection.on("error", (err) => {
+//   console.log("Failed to connect to MongoDB", err);
+// });
+// const corsOptions = {
+//   origin: process.env.CORS_ORIGIN,
+// };
+
+// app.use(cors(corsOptions));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use("/", userRoutes);
+// app.use("/admin", trainRouters);
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -7,20 +36,14 @@ const trainRouters = require("./routes/train");
 const port = 4000;
 require("dotenv").config();
 
-mongoose.connect(
-  "mongodb+srv://adityamishrad13:adityamishrad13@railyatri.land1v8.mongodb.net/?retryWrites=true&w=majority&appName=RailYatri"
-);
+mongoose.connect("mongodb://localhost:27017/railyatri");
 mongoose.connection.on("connected", () => {
   console.log("Connected to MongoDB");
 });
 mongoose.connection.on("error", (err) => {
   console.log("Failed to connect to MongoDB", err);
 });
-const corsOptions = {
-  origin: "https://railyatri.vercel.app",
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", userRoutes);
@@ -28,8 +51,3 @@ app.use("/admin", trainRouters);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-// adityamishrad13
-// WPCNi7CrCXRQVBOu
-
-// mongodb+srv://adityamishrad13:adityamishrad13@railyatri.land1v8.mongodb.net/?retryWrites=true&w=majority&appName=RailYatri
