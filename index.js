de151b944501f637ece2316d2abebfa37141f4df
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2024-06-30 00:03:18 +0530

    backemd

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const trainRouters = require("./routes/train");
 const port = 4000;
 require("dotenv").config();
 
-mongoose.connect("mongodb://localhost:27017/railyatri");
+mongoose.connect(
+  "mongodb+srv://adityamishrad13:adityamishrad13@railyatri.land1v8.mongodb.net/?retryWrites=true&w=majority&appName=RailYatri"
+);
 mongoose.connection.on("connected", () => {
   console.log("Connected to MongoDB");
 });
@@ -22,3 +24,8 @@ app.use("/admin", trainRouters);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// adityamishrad13
+// WPCNi7CrCXRQVBOu
+
+// mongodb+srv://adityamishrad13:adityamishrad13@railyatri.land1v8.mongodb.net/?retryWrites=true&w=majority&appName=RailYatri
